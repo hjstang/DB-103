2d1e95fbc459bee827690a1183dfc011221e3fa5
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2018-03-13 13:57:18 +0100

    conSQL

--- src/conSQL.java
+++ src/conSQL.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import com.mysql.jdbc.Connection;
 
 public class conSQL {
-	
+	//må legge inn url her
 	private final String dbURL= "";
 	 
 	private Connection getConnection() throws SQLException{
