ff4b60b4875dbf882a1b3c3a94c8a44feb00097f
noragk <nora.kallager@gmail.com>
2018-03-20 10:56:18 +0100

    :)

--- src/ConnectionSQL.java
+++ src/ConnectionSQL.java
@@ -15,7 +15,9 @@ public class ConnectionSQL {
             Properties p = new Properties();
             p.put(“user”,  “noragk”);
             p.put(“password”, “Norag0808”);
-            conn = DriverManager.getConnection(“jdbc:mysql://mysql.stud.ntnu.no/noragk_db_treningsdagbok?user=noragk_db&password=Norag0808")
-        }
-    }
+            conn = DriverManager.getConnection(“https://mysqladmin.it.ntnu.no/db_structure.php?server=1&db=noragk_treningsdagbok&token=57c45fd2fb6ef7d70160a6b49fd7f990“),p
+            return conn;
+        } catch (Exception e) {    
+        } throw new RuntimeException(“Unable to connect”, e);    
+    }    
 }
--- src/Treningsokt.java
+++ src/Treningsokt.java
@@ -1,4 +1,49 @@
 
 public class Treningsokt {
 
+	import java.sql.Date;
+	import java.sql.Time;
+	import java.util.ArrayList;
+	import java.util.List;
+
+	public class Workout {
+		
+		private Date dato;
+		private Time tidspunkt;
+		private int varighet;
+		private int personligForm;
+		private int prestasjon;
+		private String notat;
+		
+		
+		private List<Exercise> exerciseList;
+		
+		public Workout(Date dato, Time tidspunkt, int varighet, int personligForm, int prestasjon, String notat){
+			this.dato = dato;
+			this.tidspunkt = tidspunkt;
+			this.varighet = varighet;
+			this.personligForm = personligForm;
+			this.prestasjon = prestasjon;
+			this.notat = notat;
+			
+			this.exerciseList = new ArrayList<Exercise>();
+		}
+		
+		
+		public void addExercise(Exercise ex) {
+			this.exerciseList.add(ex);
+		}
+		
+		public Date getDato() {
+			return dato;
+		}
+			
+		
+		
+		
+
+	}
+	
+	
+	
 }
