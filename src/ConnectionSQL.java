a5758c09ee7e922e9ca83a4646745ad02f97c032
janinestang <helenejs@stud.ntnu.no>
2018-03-20 10:58:44 +0100

    --

--- src/ConnectionSQL.java
+++ src/ConnectionSQL.java
@@ -2,7 +2,6 @@ import java.sql.*;
 import java.util.Properties;
 
 public class ConnectionSQL {
-<<<<<<< HEAD
 	
 	protected Connection conn;
 	public ConnectionSQL() {
@@ -21,24 +20,3 @@ public class ConnectionSQL {
 		} catch (Exception e) {	
 		} throw new RuntimeException("Unable to connect", e);	
 	}	
-=======
-    
-    protected Connection conn;
-    public ConnectionSQL() {
-    }
-    
-    public Connection connect() {
-        try {
-            Class.forName(“com.mysql.jdbc.Driver”).newInstance();
-            
-            // Brukernavn og passord
-            Properties p = new Properties();
-            p.put(“user”,  “noragk”);
-            p.put(“password”, “Norag0808”);
-            conn = DriverManager.getConnection(“https://mysqladmin.it.ntnu.no/db_structure.php?server=1&db=noragk_treningsdagbok&token=57c45fd2fb6ef7d70160a6b49fd7f990“),p
-            return conn;
-        } catch (Exception e) {    
-        } throw new RuntimeException(“Unable to connect”, e);    
-    }    
->>>>>>> branch 'master' of https://github.com/helenejs/DB-103.git
-}
