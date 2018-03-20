6b3a8a959686cb480ac782ec8c79632311119f0f
ingriak <ingriak@stud.ntnu.no>
2018-03-20 10:58:35 +0100

    Ferdig med ovelsesgruppe klassen

--- src/ConnectionSQL.java
+++ src/ConnectionSQL.java
@@ -2,25 +2,22 @@ import java.sql.*;
 import java.util.Properties;
 
 public class ConnectionSQL {
-	
-	protected Connection conn;
-	public ConnectionSQL() {
-	}
-	
-	public Connection connect() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			
-			// Brukernavn og passord
-			Properties p = new Properties();
-			p.put("user",  "noragk");
-			p.put("password", "Norag0808");
-			conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/noragk_db_treningsdagbok?user=noragk_db&password=Norag0808")
-		}
-	}
-	
-	
-	private final String dbURL="jdbc:mysql://mysql.stud.ntnu.no/noragk_db_treningsdagbok?user=noragk_db&pa	ssword=Norag0808";
-
-	
+    
+    protected Connection conn;
+    public ConnectionSQL() {
+    }
+    
+    public Connection connect() {
+        try {
+            Class.forName(“com.mysql.jdbc.Driver”).newInstance();
+            
+            // Brukernavn og passord
+            Properties p = new Properties();
+            p.put(“user”,  “noragk”);
+            p.put(“password”, “Norag0808”);
+            conn = DriverManager.getConnection(“https://mysqladmin.it.ntnu.no/db_structure.php?server=1&db=noragk_treningsdagbok&token=57c45fd2fb6ef7d70160a6b49fd7f990“),p
+            return conn;
+        } catch (Exception e) {    
+        } throw new RuntimeException(“Unable to connect”, e);    
+    }    
 }
