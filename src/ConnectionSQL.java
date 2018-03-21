c140d4c4a549c128763786a0c137455c0fd75619
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2018-03-21 20:54:03 +0100

    Lagt til driver i pom.xml

--- src/ConnectionSQL.java
+++ src/ConnectionSQL.java
@@ -9,6 +9,7 @@ public abstract class ConnectionSQL {
 	
 	public Connection connect() {
 		try {
+			System.out.println("Trying to connect");
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			
 			// Brukernavn og passord
@@ -16,6 +17,7 @@ public abstract class ConnectionSQL {
 			p.put("user",  "noragk");
 			p.put("password", "Norag0808");
 			conn = DriverManager.getConnection("https://mysqladmin.it.ntnu.no/db_structure.php?server=1&db=noragk_treningsdagbok&token=57c45fd2fb6ef7d70160a6b49fd7f990", p);
+			System.out.println("Connection OK");
 			return conn;
 		} catch (Exception e) {	
 			throw new RuntimeException("Unable to connect", e);
